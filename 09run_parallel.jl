dd453897ad7caa7edec0998ee1844609459e50ea
Elisa Donati <elisa.donati.23@gmail.com>
2024-12-02 16:17:30 +0100

    renamic branch coherentlu with g100

--- 09run_parallel.jl
+++ 09run_parallel.jl
@@ -8,7 +8,7 @@ include("06initialize.jl")
 include("07agent_step!.jl")
 include("08simulation_step.jl")
 
-
+# foo
 # Read the CSV file
 # Name of the file in the current directory
 file_name = "all_timeseries_final_abm.csv"
